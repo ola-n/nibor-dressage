974234bdd07a5674de79db1a444baad209f8997b
Ola Nordstrom <grolch@gmail.com>
2019-10-17 23:20:41 +0200

    fix breaking prod builds

--- src/state/ReduxWrapper.js
+++ src/state/ReduxWrapper.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import { createStore as reduxCreateStore } from 'redux';
+import {
+  createStore as reduxCreateStore,
+  compose,
+  applyMiddleware,
+} from 'redux';
 import rootReducer from './';
 
-const createStore = () =>
-  reduxCreateStore(
-    rootReducer,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  );
+const middlewares = [];
+
+// if REDUX_DEVTOOLS is running, feed it with info.
+const composeEnhancers =
+  (typeof window !== 'undefined' &&
+    window &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+  compose;
+const enhancers = composeEnhancers(applyMiddleware(...middlewares));
+
+const createStore = () => reduxCreateStore(rootReducer, undefined, enhancers);
 
 export default ({ element }) => (
   <Provider store={createStore()}>{element}</Provider>
