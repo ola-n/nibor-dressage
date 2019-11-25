30ab3e6ab61a9844447eed2571bac9ba8cb92c82
Ola Nordstrom <grolch@gmail.com>
2019-11-25 20:16:52 +0100

    quicker loading of hero images

--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -133,6 +133,7 @@ export const HeroSection = ({
       <TildedImage
         imgStyle={{ objectPosition: 'left center' }}
         fluid={heroImageDesktop.childImageSharp.fluid}
+        loading="eager"
       />
     </HeroRoot>
   );
